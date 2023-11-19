65a4b8f6cfb59aa97f23dea2531c2bd3a38abd17
George Fincher <george@grimfunky.dev>
2023-11-19 10:57:04 +0000

    Feat ✨: Add public folder

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello", func (w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello from a Go program"))
-	})
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from a Go program"))
+}
 
-	err := http.ListenAndServe(":3333", nil)
+func main() {
+	server := http.NewServeMux()
+
+	server.HandleFunc("/hello", handleHello)
+
+	err := http.ListenAndServe(":3333", server)
 
 	if err == nil {
 		fmt.Println("Error while starting the server")
 	}
-
-
 }

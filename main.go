7e15bb13c6cbf21fc063b4ec5d3b04efe74a04a9
George Fincher <george@grimfunky.dev>
2023-11-19 11:36:38 +0000

    Feat ✨: Completed the server files exercise

--- main.go
+++ main.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+// Port the server is running on
+var port = ":3333"
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from a Go program"))
 }
 
-func main() {
-	server := http.NewServeMux()
+func handleStartup(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Server running at http://localhost%v/", port)
+}
 
+func main() {
+	// Create the server
+	server := http.NewServeMux()
 	server.HandleFunc("/hello", handleHello)
 
-	err := http.ListenAndServe(":3333", server)
+	// Set up the public folder
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
+
+	// Set up the error listening
+	log.Printf("Server starting at http://localhost%v/", port)
+	err := http.ListenAndServe(port, server)
+	server.HandleFunc("/", handleStartup)
+	log.Println("Hello")
 
 	if err == nil {
-		fmt.Println("Error while starting the server")
+		fmt.Printf("Error while starting the server %v", err)
 	}
 }
